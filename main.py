22580e155caeccaf3a76a0e0081a89aff4ac86d4
Kyi Wong <kyiwong97@gmail.com>
2025-03-18 19:25:11 +0800

    day3.1

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ def count_blocks(length_of_messages):
     print('余数: ', remainder)
     return (blocks + 1) if remainder < 448 else (blocks + 2)
 
+H = [0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476, 0xc3d2e1f0]
 
 # 输入字符串
+# inputMessage = 'abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz123'
 inputMessage = 'abc'
 # padding = bytearray(emessage)
 
@@ -29,19 +31,38 @@ byteMessage = inputMessage.encode()
 # 计算字符串长度 向上取整计算byte
 lengthOfMessages = len(byteMessage) * 8
 # 448bits边界测试
-# lengthOfMessages = 55 * 8
+# lengthOfMessages = 56 * 8
 print('length of bits: ', lengthOfMessages)
-
-# 补位，如果bit流需要修改这里
-# 其实知道lengthOfMessages可以优化这里
-byteMessage += b'\x80'
-if (len(byteMessage) % 64 != 56):
-    pass
 
 # 分块
 blockNumber = count_blocks(lengthOfMessages)
 # blockNumber = count_blocks(1000)
 print('blocks: ', blockNumber)
 
-# 补零
+# 补位，如果bit流需要修改这里
+# 首位补1 其余补0
+byteMessage += b'\x80'
+zeroBits = (blockNumber * 512 - 64 - 8 - lengthOfMessages)
+byteMessage += b'\x00' * (zeroBits // 8)
+# 补长度 需要把长度变为16进制
+# 输出长度为8bytes，大端在前的64bits消息长度
+byteMessage += lengthOfMessages.to_bytes(8, byteorder='big')
+
+W = []
+for t in range(0, 79):
+    if t < 16:
+        W.append(int.from_bytes(byteMessage[ t * 4 : ( t + 1 ) * 4 ]))
+    else:
+        W.append(W[t - 3] ^ W[t - 8] ^ W[t - 14] ^ W[t - 16] << 1)
+
+# print W
+# for i in range(0, 79):
+#     print(hex(W[i]) ,end=' ')
+#     if (i + 1) % 4 == 0:
+#         print()
+# print()
+for t in range(0, blockNumber):
+    a = H[0], b = H[1], c = H[2], d = H[3], e = H[4]
+    H[0] = a + H[0] >> 32 
+print(byteMessage)
 
--- test.py
+++ test.py
@@ -1,15 +1,5 @@
-import time
-
-x = 1234567891234567891234567895768687997978
-
-# 使用 >> 操作
-start_time = time.time()
-for _ in range(100000000):
-    result_shift = x >> 9
-print("Time for >>:", time.time() - start_time)
-
-# 使用 // 操作
-start_time = time.time()
-for _ in range(100000000):
-    result_divide = x // 512
-print("Time for //:", time.time() - start_time)
+test = b'abc\x80'
+test2 = bytearray(test)
+test2.append(0)
+test2.append(0x33)
+print(test2)
